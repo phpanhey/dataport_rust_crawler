e376e2ec54996537138771f59955464141b055d2
Philipp Panhey <philipp.panhey@gmail.com>
2022-10-31 15:11:32 +0100

    now extracting job urls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 use reqwest::Error;
+use select::document::Document;
+use select::predicate::Name;
+use std::env;
+
 fn main() {
-    let res = get_html("https://www.rust-lang.org");
-    println!("{:?}", res);
+    let args: Vec<String> = env::args().collect();
+    let _pattern = &args[1];
+    let urls = &args[2..args.len()];
+
+    let hrefs = extract_urls(urls);
+    println!("{:?}",hrefs);
 }
 
 pub fn get_html(url: &str) -> Result<String, Error> {
     return reqwest::blocking::get(url)?.text();
+}
+
+pub fn extract_urls(urls: &[String]) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
+    for url in urls {
+        let html = get_html(url);
+        let document = Document::from(html.unwrap().as_str());
+        for item in document.find(Name("a")) {
+            let elem = item.attr("href");
+            if elem != None && is_candidate(elem) {
+                res.push(elem.unwrap().to_string());
+            }
+        }
+    }
+    return res;
+}
+
+pub fn is_candidate(url_option: Option<&str>) -> bool {
+    let url = url_option.unwrap();
+    return url.contains("karriere.")
+        && url.split("/").collect::<Vec<&str>>()[3]
+            .chars()
+            .next()
+            .unwrap()
+            .is_uppercase();
 }
